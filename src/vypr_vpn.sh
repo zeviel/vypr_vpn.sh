5a5a18a1dde79f33df5c6e864607a737b0d978a9
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:12:17 +0800

    Update vypr_vpn.sh

--- src/vypr_vpn.sh
+++ src/vypr_vpn.sh
@@ -2,16 +2,21 @@
 
 api="https://api.goldenfrog.com"
 validation_api="https://validation.api.goldenfrog.com"
+product="VyprVPN"
+platform="Android"
+platform_version=9
+version="4.5.2.113841"
+user_agent="okhttp/3.14.7"
 
 function get_self_info() {
 	curl --request GET \
-		--url "$validation_api/vyprvpn/connections/self" \
-		--user-agent "okhttp/3.14.7" \
+		--url "$validation_api/$product/connections/self" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
-		--header "x-gf-platform: Android" \
-		--header "x-gf-platform-version: 9" \
-		--header "x-gf-product: VyprVPN" \
-		--header "x-gf-product-version: 4.5.2.113841"
+		--header "x-gf-platform: $platform" \
+		--header "x-gf-platform-version: $platform_version" \
+		--header "x-gf-product: $product" \
+		--header "x-gf-product-version: $version"
 }
 
 function register() {
@@ -21,12 +26,12 @@ function register() {
 	# 4 - password: (string): <password>
 	curl --request POST \
 		--url "$api/platform-gf/api/actions/signup/create-principal" \
-		--user-agent "okhttp/3.14.7" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
-		--header "x-gf-platform: Android" \
-		--header "x-gf-platform-version: 9" \
-		--header "x-gf-product: VyprVPN" \
-		--header "x-gf-product-version: 4.5.2.113841" \
+		--header "x-gf-platform: $platform" \
+		--header "x-gf-platform-version: $platform_version" \
+		--header "x-gf-product: $product" \
+		--header "x-gf-product-version: $version" \
 		--data '{
 			"communication_locale": "'${1:-ru-RU}'",
 			"email": "'$2'",
@@ -41,12 +46,12 @@ function get_id_token() {
 	# 2 - password: (string): <password>
 	response=$(curl --request POST \
 		--url "$api/platform-gf/oidc/token" \
-		--user-agent "okhttp/3.14.7" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/x-www-form-urlencoded" \
-		--header "x-gf-platform: Android" \
-		--header "x-gf-platform-version: 9" \
-		--header "x-gf-product: VyprVPN" \
-		--header "x-gf-product-version: 4.5.2.113841" \
+		--header "x-gf-platform: $platform" \
+		--header "x-gf-platform-version: $platform_version" \
+		--header "x-gf-product: $product" \
+		--header "x-gf-product-version: $version" \
 		--data "client_id=ph-mobile&client_secret=ph-mobile-secret&grant_type=password&username=$1&password=$2&scope=openid profile groups tenant")
 	if [ -n $(jq -r ".id_token" <<< "$response") ]; then
 		id_token=$(jq -r ".id_token" <<< "$response")
@@ -58,21 +63,21 @@ function get_offering_details() {
 	# 1 - offering_id: (string): <offering_id>
 	curl --request GET \
 		--url "$api/platform-gf/api/product/offerings/$1/actions/get-details" \
-		--user-agent "okhttp/3.14.7" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
-		--header "x-gf-platform: Android" \
-		--header "x-gf-platform-version: 9" \
-		--header "x-gf-product: VyprVPN" \
-		--header "x-gf-product-version: 4.5.2.113841"
+		--header "x-gf-platform: $platform" \
+		--header "x-gf-platform-version: $platform_version" \
+		--header "x-gf-product: $product" \
+		--header "x-gf-product-version: $version"
 }
 
 function get_locations() {
 	curl --request GET \
-		--url "$api/vyprvpn/locations-unauthenticated" \
-		--user-agent "okhttp/3.14.7" \
+		--url "$api/$product/locations-unauthenticated" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
-		--header "x-gf-platform: Android" \
-		--header "x-gf-platform-version: 9" \
-		--header "x-gf-product: VyprVPN" \
-		--header "x-gf-product-version: 4.5.2.113841"
+		--header "x-gf-platform: $platform" \
+		--header "x-gf-platform-version: $platform_version" \
+		--header "x-gf-product: $product" \
+		--header "x-gf-product-version: $version"
 }
